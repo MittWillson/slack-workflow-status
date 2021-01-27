84d508b769e5dbad7c120207797292e3a67bdae5
Mitt <10653144+MittWillson@users.noreply.github.com>
2021-01-27 14:48:46 +0800

    fix: commit commit url

--- dist/index.js
+++ dist/index.js
@@ -3455,11 +3455,11 @@ async function main() {
     const repo_url = "<https://github.com/" + workflow_run.data.repository.full_name + "|*" + workflow_run.data.repository.full_name + "*>";
     const branch_url = "<https://github.com/" + workflow_run.data.repository.full_name + "/tree/" + branch + "|*" + branch + "*>";
     const workflow_run_url = "<" + workflow_run.data.html_url + "|#" + workflow_run.data.run_number + ">";
-    const commit_url = `<https://github.com/${workflow_run.data.repository.full_name}/commit/${workflow_run.data.head_commit.id}>|*${workflow_run.data.head_commit.id.substr(0, 7)}*>`;
+    const commit_url = `<https://github.com/${workflow_run.data.repository.full_name}/commit/${workflow_run.data.head_commit.id}|*${workflow_run.data.head_commit.id.substr(0, 7)}*>`;
     // Example: Success: AnthonyKinson's `push` on `master` for pull_request
     let status_string = workflow_msg + " " + actor + "'s `" + event + "` on `" + branch_url + "`" + `@${commit_url}\n`;
     // Example: Workflow: My Workflow #14 completed in `1m 30s`
-    const commit_log = `Commit log: ${workflow_run.data.head_commit.message}\n`;
+    const commit_log = `Commit Message: ${workflow_run.data.head_commit.message}\n`;
     const details_string = "Workflow: " + workflow_name + " " + workflow_run_url + " completed in `" + workflow_duration + "`";
     // Build Pull Request string if required
     let pull_requests = "";
--- src/main.ts
+++ src/main.ts
@@ -139,11 +139,11 @@ async function main(){
   const repo_url: string = "<https://github.com/" + workflow_run.data.repository.full_name + "|*"+ workflow_run.data.repository.full_name +"*>"
   const branch_url: string = "<https://github.com/"+workflow_run.data.repository.full_name+"/tree/"+branch+"|*"+branch+"*>"
   const workflow_run_url: string = "<"+workflow_run.data.html_url+"|#"+workflow_run.data.run_number+">"
-  const commit_url: string = `<https://github.com/${workflow_run.data.repository.full_name}/commit/${workflow_run.data.head_commit.id}>|*${workflow_run.data.head_commit.id.substr(0,7)}*>`
+  const commit_url: string = `<https://github.com/${workflow_run.data.repository.full_name}/commit/${workflow_run.data.head_commit.id}|*${workflow_run.data.head_commit.id.substr(0,7)}*>`
   // Example: Success: AnthonyKinson's `push` on `master` for pull_request
   let status_string: string = workflow_msg+" "+actor+"'s `"+event+"` on `"+branch_url+"`"+`@${commit_url}\n`
   // Example: Workflow: My Workflow #14 completed in `1m 30s`
-  const commit_log: string = `Commit log: ${workflow_run.data.head_commit.message}\n`;
+  const commit_log: string = `Commit Message: ${workflow_run.data.head_commit.message}\n`;
   const details_string: string = "Workflow: "+workflow_name+" "+workflow_run_url+" completed in `"+ workflow_duration+"`"
   // Build Pull Request string if required
   let pull_requests = ""
